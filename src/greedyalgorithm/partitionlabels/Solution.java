8c2b257c316b072cd239cb88cf4e09b2fa5d4a8f
yanming <463328375@qq.com>
2020-02-25 23:33:14 +0800

    修改说明：合入贪心算法，划分字母区间。 修改人：yanming

--- src/greedyalgorithm/partitionlabels/Solution.java
+++ src/greedyalgorithm/partitionlabels/Solution.java
@@ -18,15 +18,17 @@ public class Solution {
 	public List<Integer> partitionLabels(String S) {
 		int length = S.length();
 		int[] lastIndexOfChar = new int[26];
+		// 找到字符在字符串中的最后的位置的索引，然后存储在数组中。
 		for (int i = 0; i < length; i++) {
 			int j = S.charAt(i) - 'a';
 			lastIndexOfChar[j] = i;
 		}
-		
+
 		List<Integer> partitions = new ArrayList<>();
 		int firstIndex = 0;
 		while (firstIndex < length) {
 			int lastIndex = firstIndex;
+			// 如果字符在后面都没有出现过了，说明后面没有了，那么此时就不需要进行循环了，重新开始寻找。
 			for (int i = firstIndex; i < length && i <= lastIndex; i++) {
 				int index = lastIndexOfChar[S.charAt(i) - 'a'];
 				if (index > lastIndex) {
@@ -38,7 +40,7 @@ public class Solution {
 		}
 		return partitions;
 	}
-	
+
 	public static void main(String[] args) {
 		Solution solution = new Solution();
 		solution.partitionLabels("ababcbacadefegdehijhklij");
